041f59c7e88084c91043411dc13cb71069aa4597
Jadhav <2091442@cognizant.com>
2024-05-29 21:35:57 +0530

    Code change

--- firstcode.c
+++ firstcode.c
@@ -8,7 +8,7 @@ int main() {
 
     // Generate a random number between 0 and RAND_MAX
     int value = rand();
-
+ 
     printf("The Random Value is: %d\n", value);
 
     // Generate 10 random numbers
@@ -34,3 +34,4 @@ int main() {
 
     return 0;
 }
+Shreyas Jadhav
